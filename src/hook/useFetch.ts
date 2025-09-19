19ba42af58a18339eac35840465511766bb1b234
mohamim360 <mohamim360@gmail.com>
2025-09-19 10:27:39 +0600

    added custom hook to handle API call

---
+++ src/hook/useFetch.ts
@@ -0,0 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useFetch<T>(url: string | null) {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+	// Fetch data when URL changes
+  useEffect(() => {
+    if (!url) return;
+
+    setLoading(true);
+    setError(null);
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok) throw new Error("Failed to fetxh data");
+        return response.json();
+      })
+      .then((data: T) => setData(data))
+      .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
+  }, [url]);
+
+  return { data, loading, error };
+}
